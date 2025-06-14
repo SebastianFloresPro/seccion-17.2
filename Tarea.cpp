20fc70fc7986f8da69325815bbaf2d45e5b9e928
Sebastian Flores <167713089+SebastianFloresPro@users.noreply.github.com>
2025-06-13 20:59:20 -0500

    Update Tarea.cpp

--- Tarea.cpp
+++ Tarea.cpp
@@ -21,31 +21,32 @@ int areaCuadrado(int lado, int )
 {
 	return lado * lado;
 }
-	
-	
+
+
 void ejecutar(int (*Arrpf)(int, int), int ArrPar[], std::string nombres[]) 
 {
 	
 	std::cout << "Area del " << nombres[0] << ": "; 
-	std::cout << funcion(params[0], params[1]) << std::endl;
+	std::cout << Arrpf(ArrPar[0], ArrPar[1]) << std::endl;
 }
-	
+
 int main() {
-		
+	
 	int paramsRectangulo[] = {5, 3};  
 	int paramsTriangulo[] = {5, 3};   
 	int paramsCirculo[] = {5, 0};    
 	int paramsCuadrado[]={5, 5};     
-		
-		
+	
+	
 	std::string n[] = {"rectángulo", "triángulo", "circulo", "cuadrado"};
-		
-		
+	
+	
 	
 	ejecutar(areaRectangulo, paramsRectangulo, n);
 	ejecutar(areaTriangulo, paramsTriangulo, n+1); 
 	ejecutar(areaCirculo, paramsCirculo, n+2);     
 	ejecutar(areaCuadrado, paramsCuadrado,n+3);
-		
-		
+	
+	
 }
+
